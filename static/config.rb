c411305a046ba6f4f770814636fbbfe24c5e5659
Hallgeir Lien <hallgeir@server9.subsys.no>
2012-08-25 14:34:59 +0200

    Initial commit

---
+++ static/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+require 'susy'
+
+# Project directories
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+# CSS generation
+#output_style = :compressed
+output_style = :expanded
+line_comments = false
+relative_assets = true
---
+++ static/fonts/redcircle/readme.txt
@@ -0,0 +1,8 @@
+Red Circle computer font ©2000 Harold Lohner
+
+FREEWARE-Distribute freely.
+
+Based on the lettering formerly used on Eight O'Clock brand coffees.
+
+HLohner@aol.com
+http://members.aol.com/fontner
